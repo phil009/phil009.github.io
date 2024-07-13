49397b01cc00b09d45a1ad7a25cbca8fc807fead
phil009 <philipogboh60@gmail.com>
2024-07-13 12:20:37 +0100

    new update

---
+++ cinema-site/src/assets/js/movies.js
@@ -0,0 +1,9 @@
+const movies = {
+    movie1: {
+        title: 'Zack Synders Justice League',
+        thumbnail: '../images/justice-league-thumb.png',
+        background: '../images/justice-league-bg.png'
+    }
+}
+
+document.querySelector('.hero .featured')
